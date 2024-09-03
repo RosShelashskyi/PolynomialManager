9644a74b11175ad62f47fe0c5b34a3801c9bb2fc
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-09-03 14:18:23 -0500

    Added input check

--- src/main/java/edu/tcu/rossshelashskyi/polynomialmanager/PolyManager.kt
+++ src/main/java/edu/tcu/rossshelashskyi/polynomialmanager/PolyManager.kt
@@ -1,6 +1,5 @@
 package edu.tcu.rossshelashskyi.polynomialmanager;
 
-
 fun main(){
     while(true){
         val command = readlnOrNull()
@@ -10,12 +9,16 @@ fun main(){
         if (args != null) {
             if(args[0] == "INSERT"){
 
+
             }else if(args[0] == "DELETE"){
 
             }else if(args[0] == "SEARCH"){
 
             }else if(args[0] == "QUIT") {
                 return
+            }
+            else{
+                println("INCORRECT INPUT. ACCEPTABLE COMMANDS:\nINSERT\nDELETE\nSEARCH\nQUIT")
             }
         }
     }
