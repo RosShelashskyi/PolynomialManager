05aba0a36a2ca3b1a682732c8f9bc8f71b33006b
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-09-05 14:49:54 -0500

    Formatting

--- src/main/java/edu/tcu/rossshelashskyi/polynomialmanager/PolyManager.kt
+++ src/main/java/edu/tcu/rossshelashskyi/polynomialmanager/PolyManager.kt
@@ -1,44 +1,51 @@
 package edu.tcu.rossshelashskyi.polynomialmanager;
 
-fun main(){
-    val polyList = PolyList()
-    while(true){
-        //parse the user input
-        val command = readlnOrNull()
+class PolyManager{
+    fun main(){
+        val polyList = PolyList()
+        while(true){
+            //parse the user input
+            val command = readlnOrNull()
 
-        val args: List<String>? = command?.split(" ")
+            val args: List<String>? = command?.split(" ")
 
-        if (args != null) {
-            //check which command is entered
-            if(args[0] == "INSERT"){
-                //set up the polynomial object
-                val polynomial = Polynomial(args[1])
-                //iterate over the terms in the input
-                var i = 2
-                while(i < args.size){
-                    //split term into integers
-                    val termStr: List<String> = args[i].split(",")
-                    //create a term object with coefficient and exponents from the input string
-                    val term = Term(termStr[0].toInt(), termStr[1].toInt(), termStr[2].toInt(), termStr[3].toInt())
-                    //add term to the polynomial
-                    polynomial.addTerm(term)
-                    i++
+            if (args != null) {
+                //check which command is entered
+                if(args[0] == "INSERT"){
+                    //set up the polynomial object
+                    val polynomial = Polynomial(args[1])
+                    //iterate over the terms in the input
+                    var i = 2
+                    while(i < args.size){
+                        //split term into integers
+                        val termStr: List<String> = args[i].split(",")
+                        //create a term object with coefficient and exponents from the input string
+                        val term = Term(termStr[0].toInt(), termStr[1].toInt(), termStr[2].toInt(), termStr[3].toInt())
+                        //add term to the polynomial
+                        polynomial.addTerm(term)
+                        i++
+                    }
+                    //add the polynomial to polyList
+                    polyList.insert(polynomial)
+                }else if(args[0] == "DELETE"){
+                    //delete polynomial with name in the input
+                    polyList.delete(args[1])
+                }else if(args[0] == "SEARCH"){
+                    //search for polynomial
+                    polyList.search(args[1])
+                }else if(args[0] == "QUIT") {
+                    return
                 }
-                //add the polynomial to polyList
-                polyList.insert(polynomial)
-            }else if(args[0] == "DELETE"){
-                //delete polynomial with name in the input
-                polyList.delete(args[1])
-            }else if(args[0] == "SEARCH"){
-                //search for polynomial
-                polyList.search(args[1])
-            }else if(args[0] == "QUIT") {
-                return
-            }
-            else{
-                //if the input is incorrect, print error message
-                println("INCORRECT INPUT. ACCEPTABLE COMMANDS:\nINSERT\nDELETE\nSEARCH\nQUIT")
+                else{
+                    //if the input is incorrect, print error message
+                    println("INCORRECT INPUT. ACCEPTABLE COMMANDS:\nINSERT\nDELETE\nSEARCH\nQUIT")
+                }
             }
         }
     }
+}
+
+fun main(){
+    val polyManager = PolyManager()
+    polyManager.main()
 }
